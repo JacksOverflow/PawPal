e34ba7dc0793c71a5632ac814cc1b5a1950e1cfc
JacksOverflow <rjwebb91@gmail.com>
2022-04-11 16:20:33 +0100

    header/footer changes

---
+++ components/footer.js
@@ -0,0 +1,12 @@
+
+export default function Footer() {
+    return (
+        <footer>
+        <a>
+          <span>
+            <new Image src="/PawPalLogo.jpg" alt="PawPal Logo" width={80} height={60} />
+          </span>
+        </a>
+      </footer>
+    )
+  }
